b9c7d2171e8fb13ba1e5c83128f0bb39c5b030eb
markcor11 <mark.cornaia@uk.ibm.com>
2020-05-05 15:43:18 +0100

    Call api to turn off MPMetrics Auth
    Signed-off-by: Mark Cornaia <mark.cornaia@uk.ibm.com>

--- src/performance/dashboard/src/App.js
+++ src/performance/dashboard/src/App.js
@@ -44,7 +44,6 @@ function App() {
     <SocketContext.Provider value={socket}>
       <div className="App">
         <Fragment>
-          {/*  <GridDebug />  */}
           <ErrorBoundary>
             <NavBar projectID={projectID} />
           </ErrorBoundary>
